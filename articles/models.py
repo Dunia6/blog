1b069790d5a7b502c67c4051c44df5d49507c5d1
Jephte Dunia <jephtedunia07@gmail.com>
2022-10-07 23:07:36 +0200

    Initial commit

---
+++ articles/models.py
@@ -0,0 +1,51 @@
+from email.policy import default
+from django.db import models
+from django.contrib.auth.models import User
+# Create your models here.
+
+class Category(models.Model):
+    title = models.CharField(max_length=100)
+    created_on = models.DateTimeField(auto_now_add = True)
+
+    def __str__(self):
+        return self.title
+
+
+CHOIX_STATUS = (
+    ("brouillon", "Brouillons"),
+    ("publié", "Publié"),
+)
+class Article(models.Model):
+    title = models.CharField(max_length=100, blank=True, unique=True)
+    slug = models.SlugField(max_length=200,blank=True, unique=True)
+    image = models.ImageField(upload_to="uploads/images/{0}",null=True, blank='True')
+    overview = models.TextField()
+    content = models.TextField()
+    status = models.CharField(choices=CHOIX_STATUS, max_length=200, default="brouillon")
+    updated_on = models.DateTimeField(auto_now=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ['-created_on']
+
+
+    def __str__(self):
+        return self.title
+
+
+
+class Comment(models.Model):
+    article = models.ForeignKey(Article, on_delete=models.CASCADE, related_name="comments")
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    body = models.TextField()
+    active = models.BooleanField(default=False)
+    created_on = models.DateTimeField(auto_now_add=True)
+
+
+    class Meta:
+        ordering = ["-created_on"]
+    
+    def __str__(self):
+        return "Commentaire {}, commenté par {}".format(self.body, self.name)
